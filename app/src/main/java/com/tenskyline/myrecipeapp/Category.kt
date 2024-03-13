fc8b2a66e7a68f5dd409fee308e729efacec4963
TenSkyline <rayvaldokncc@gmail.com>
2024-03-13 14:21:06 +0700

    add data class Category & CategoryResponse

---
+++ app/src/main/java/com/tenskyline/myrecipeapp/Category.kt
@@ -0,0 +1,10 @@
+package com.tenskyline.myrecipeapp
+
+data class Category(
+    val idCategory: String,
+    val strCategory: String,
+    val strCategoryThumb: String,
+    val strCategoryDescription: String
+)
+
+data class CategoryResponse(val categories: List<Category>)
